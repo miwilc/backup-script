e76f4421ef77699514408c7b263f892114ec3cef
Andre Bongon <andremarx13@gmail.com>
2016-08-20 23:56:22 +0900

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Backup Script
+#   Author: Andre Bongon (1Conan)
+#   Copyright (c) 2016 Andre Bongon
+#   Licensed under MIT License
+#
+#
+
+source ./options.sh
+
+deleteoldbackups() {
+  true;
+}
+
+mysqlbackup() {
+  mysqldump -uroot -p${MYSQLPASS} --alldatabases > ${TMPDIR}my.sql
+  tar -cvf ${TMPDIR}mysql.tar ${TMPDIR}my.sql
+  
+  if [ "$ENPASS" = false ]; then
+	7z a -y -sdel -mk=7 ${TMPDIR}mysql.tar.7z ${TMPDIR}mysql.tar
+  elif [ "$ENPASS" = true ]; then
+	7z 7z a -y -sdel -p"${PASS}" -mk=7 ${TMPDIR}mysql.tar.7z ${TMPDIR}mysql.tar
+  fi
+  
+  mv ${TMPDIR}mysql.tar.7z ${TMPDIR}backup
+}
+
+if [ "$ENPASS" = true ]; then
+	PASS="$(openssl rand -hex 32 | head -c 16)$(openssl rand -hex 32 | tail -c 16)"
+fi
+
+mkdir "${TMPDIR}backup"
+
+for i in "${DIR[@]}"; do
+  FILENAME=$(echo ${i} | sed 's/^.//' | sed 's/\//./g')
+  tar -cvf "${TMPDIR}${FILENAME}.tar" $i
+  if [ "$ENPASS" = false ]; then
+	7z a -y -sdel -mx=7 "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
+  elif [ "$ENPASS" = true ]; then
+	7z a -y -sdel -p"${PASS}" -mx=7 "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
+  fi
+  mv "${TMPDIR}${FILENAME}.tar.7z" ${TMPDIR}backup
+done
+
+mysqlbackup
+
+FILENAME=$(date + "%m-%d-%y")
+
+if [ "$ENPASS" = true ]; then
+	echo "${PASS}" > ${TMPDIR}backup/key.bin
+	openssl rsautl -encrypt -inkey ${PUBLICKEY} -pubin -in "${TMPDIR}backup/key.bin" -out "${TMPDIR}backup/key.bin.enc"
+fi
+
+7z a -y -sdel -mk=7 "${TMPDIR}${ID}-${FILENAME}.tar.7z" "${TMPDIR}backup/"
+
+mv "${TMPDIR}${ID}-${FILENAME}.tar.7z" ${CLOUDDIR}
+
+deleteoldbackups
