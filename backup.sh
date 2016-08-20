0bb5f36fc48d4950752efaed89cae8328d04f2f1
Andre Bongon <andremarx13@gmail.com>
2016-08-21 00:11:17 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -21,16 +21,16 @@ deleteoldbackups() {
 }
 
 mysqlbackup() {
-	mysqldump -uroot -p${MYSQLPASS} --alldatabases > ${TMPDIR}my.sql
-	tar -cvf ${TMPDIR}mysql.tar ${TMPDIR}my.sql
+	mysqldump -uroot -p"${MYSQLPASS}" --alldatabases > "${TMPDIR}my.sql"
+	tar -cvf "${TMPDIR}mysql.tar" "${TMPDIR}my.sql"
   
 	if [ "$ENPASS" = false ]; then
-		7z a -y -sdel -mk=7 ${TMPDIR}mysql.tar.7z ${TMPDIR}mysql.tar
+		7z a -y -sdel -mk=7 "${TMPDIR}mysql.tar.7z" "${TMPDIR}mysql.tar"
 	elif [ "$ENPASS" = true ]; then
-		7z 7z a -y -sdel -p"${PASS}" -mk=7 ${TMPDIR}mysql.tar.7z ${TMPDIR}mysql.tar
+		7z 7z a -y -sdel -p"${PASS}" -mk=7 "${TMPDIR}mysql.tar.7z" "${TMPDIR}mysql.tar"
 	fi
   
-	mv ${TMPDIR}mysql.tar.7z ${TMPDIR}backup
+	mv "${TMPDIR}mysql.tar.7z" "${TMPDIR}backup"
 }
 
 if [ "$ENPASS" = true ]; then
@@ -40,14 +40,14 @@ fi
 mkdir "${TMPDIR}backup"
 
 for i in "${DIR[@]}"; do
-	FILENAME=$(echo ${i} | sed 's/^.//' | sed 's/\//./g')
-	tar -cvf "${TMPDIR}${FILENAME}.tar" $i
+	FILENAME=$(echo "${i}" | sed 's/^.//' | sed 's/\//./g')
+	tar -cvf "${TMPDIR}${FILENAME}.tar" "$i"
 	if [ "$ENPASS" = false ]; then
 		7z a -y -sdel -mx=7 "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
 	elif [ "$ENPASS" = true ]; then
 		7z a -y -sdel -p"${PASS}" -mx=7 "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
 	fi
-	mv "${TMPDIR}${FILENAME}.tar.7z" ${TMPDIR}backup
+	mv "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}backup"
 done
 
 mysqlbackup
@@ -55,12 +55,12 @@ mysqlbackup
 FILENAME=$(date + "%m-%d-%y")
 
 if [ "$ENPASS" = true ]; then
-	echo "${PASS}" > ${TMPDIR}backup/key.bin
-	openssl rsautl -encrypt -inkey ${PUBLICKEY} -pubin -in "${TMPDIR}backup/key.bin" -out "${TMPDIR}backup/key.bin.enc"
+	echo "${PASS}" > "${TMPDIR}backup/key.bin"
+	openssl rsautl -encrypt -inkey "${PUBLICKEY}" -pubin -in "${TMPDIR}backup/key.bin" -out "${TMPDIR}backup/key.bin.enc"
 fi
 
 7z a -y -sdel -mk=7 "${TMPDIR}${ID}-${FILENAME}.tar.7z" "${TMPDIR}backup/"
 
-mv "${TMPDIR}${ID}-${FILENAME}.tar.7z" ${CLOUDDIR}
+mv "${TMPDIR}${ID}-${FILENAME}.tar.7z" "${CLOUDDIR}"
 
 deleteoldbackups
