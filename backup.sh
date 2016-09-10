f8217eb056c63caa1b85c48f894866ff6e8092c6
Andre Bongon <andremarx13@gmail.com>
2016-09-10 19:35:55 +0900

    Fix if statements

--- backup.sh
+++ backup.sh
@@ -23,16 +23,16 @@ mysqlbackup() {
 	mysqldump -uroot -p"${MYSQLPASS}" --all-databases > ${TMPDIR}my.sql
 	tar -cvf "${TMPDIR}mysql.tar" "${TMPDIR}my.sql"
   
-	if [ "$ENPASS" = false ]; then
+	if [ "$ENPASS" == false ]; then
 		7z a -y -sdel "${TMPDIR}mysql.tar.7z" "${TMPDIR}mysql.tar"
-	elif [ "$ENPASS" = true ]; then
+	elif [ "$ENPASS" == true ]; then
 		7z 7z a -y -sdel -p"${PASS}" "${TMPDIR}mysql.tar.7z" "${TMPDIR}mysql.tar"
 	fi
   
 	mv "${TMPDIR}mysql.tar.7z" "${TMPDIR}backup"
 }
 
-if [ "$ENPASS" = true ]; then
+if [ "$ENPASS" == true ]; then
 	PASS="$(openssl rand -hex 32 | head -c 16)$(openssl rand -hex 32 | tail -c 16)"
 fi
 
@@ -41,9 +41,9 @@ mkdir "${TMPDIR}backup"
 for i in "${DIR[@]}"; do
 	FILENAME=$(echo "${i}" | sed 's/^.//' | sed 's/\//./g')
 	tar -cvf "${TMPDIR}${FILENAME}.tar" "$i"
-	if [ "$ENPASS" = false ]; then
+	if [ "$ENPASS" == false ]; then
 		7z a -y -sdel "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
-	elif [ "$ENPASS" = true ]; then
+	elif [ "$ENPASS" == true ]; then
 		7z a -y -sdel -p"${PASS}" "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}${FILENAME}.tar"
 	fi
 	mv "${TMPDIR}${FILENAME}.tar.7z" "${TMPDIR}backup"
@@ -54,7 +54,7 @@ fi
 
 FILENAME=$(date + "%m-%d-%y")
 
-if [ "$ENPASS" = true ]; then
+if [ "$ENPASS" == true ]; then
 	echo "${PASS}" > "${TMPDIR}backup/key.bin"
 	openssl rsautl -encrypt -inkey "${PUBLICKEY}" -pubin -in "${TMPDIR}backup/key.bin" -out "${TMPDIR}backup/key.bin.enc"
 fi
