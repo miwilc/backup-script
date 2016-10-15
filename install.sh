d62052ec8902dc961295c400408da26e70a1e9a5
Andre Bongon <andremarx13@gmail.com>
2016-10-15 12:08:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,22 +15,31 @@ if [[ "$resp" == "y" || "$resp" == "Y" ]]; then
 else
 	ENABLE_MYSQL=false
 fi
+TMP_COUNT=0
+while true; do
+	read -r -p "Enter directory to be backed up (Type done when done) [$(expr ${TMP_COUNT} + 1)]: " DIR_PATH
+	if [ -f "${DIR_PATH}" ]; then
+		echo "Not a directory!"
+		exit
+	elif [ -d "${DIR_PATH}" ]; then
+		DIRS="DIR[${TMP_COUNT}]=${DIR_PATH};${DIRS}"
+	else [ "${DIR_PATH}" == "done" ]; then
+		break
+	fi
+done
 
 read -r -p "Enter file path where backups will be stored (Include trailing slash): " backup_path
 
 mkdir /usr/local/backup-settings
 cat <<EOF > /usr/local/backup-settings/options.sh
-ID="$id_name"
-MYSQLPASS="$msql_rootpass"
+ID="${id_name}"
+MYSQLPASS="${msql_rootpass}"
 PUBLICKEY=""
 TMPDIR="/tmp/"
-CLOUDDIR="$backup_path"
+CLOUDDIR="${backup_path}"
 
-Folders to backup
-#DIR[0]="/home/"
-#DIR[1]="/root/"
-#DIR[2]="/var/www/"
-#DIR[3]="/usr/local/nginx/"
+#Folders to backup
+${DIRS}
 EOF
 
 echo "Please edit /usr/local/backup-settings/options.sh and add the paths to be backed up."
